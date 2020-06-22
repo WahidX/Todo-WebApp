35b8a8cf6e5ebccd5a7b61e43fbf3c72a3e74d99
WahidX <wahidx93@gmail.com>
2020-06-22 12:27:28 +0530

    blank string handled

--- index.js
+++ index.js
@@ -29,6 +29,11 @@ function clearAll(){
 
 function addNote(){
     
+    if (inputText.value.trim == ""){
+        window.alert("Nothing is typed");
+        return;
+    }
+    
     const noteDiv = document.createElement("div");
     noteDiv.classList.add("note");
     noteDiv.innerHTML = "<input type=\"checkbox\" id="+index+" class=\"note-input\" onclick=\"checkClicked(this)\"><label for="+ index +">"+ inputText.value +"</label><button onclick=\"deleteNote(this)\"><i class=\"fas fa-times\"></i></button>";
@@ -54,8 +59,9 @@ function checkClicked(ele){
 }
 
 function deleteNote(ele){
+    if(ele.parentNode.childNodes[0].checked != true)
+        updateCount(--count);
     ele.parentElement.remove();
-    updateCount(--count);
 }
 
 function updateCount(count){
